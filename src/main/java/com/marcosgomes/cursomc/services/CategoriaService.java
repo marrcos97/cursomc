3aaf5440bbd4299c6b7c6fb320805323a880c95c
Marcos Vinicius Gomes <marcos.vgomes@sp.senac.br>
2019-02-01 15:13:39 -0200

    Criando repository e service para Categoria

---
+++ src/main/java/com/marcosgomes/cursomc/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.marcosgomes.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marcosgomes.cursomc.domain.Categoria;
+import com.marcosgomes.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository repo;
+
+    public Categoria buscar(Integer id) {
+        final Optional<Categoria> obj = repo.findById(id);
+        return obj.orElse(null);
+    }
+
+}
