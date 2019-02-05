aff1c3a31b376050154150a880c4fcaf197c6377
Marcos Vinicius Gomes <marcos.vgomes@sp.senac.br>
2019-02-05 17:09:58 -0200

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/marcosgomes/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.marcosgomes.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marcosgomes.cursomc.domain.Cliente;
+import com.marcosgomes.cursomc.exceptions.ObjectNotFoundException;
+import com.marcosgomes.cursomc.repositories.ClienteRepository;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository repo;
+
+    public Cliente buscar(Integer id) {
+        final Optional<Cliente> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + " Tipo: " + Cliente.class.getName()));
+    }
+
+}
